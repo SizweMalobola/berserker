b9d2dbbef5d1cb0dfd074e7a050638b5bf9772b8
SizweMalobola <25reyals@gmail.com>
2021-11-19 12:24:10 +0200

    Add Quote section UI and style module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import QuoteSection from "./components/quote/QuoteSection";
 
 export class App extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ export class App extends Component {
       <>
         <Navbar />
         <Hero />
+        <QuoteSection />
       </>
     );
   }
---
+++ src/components/quote/QuoteSection.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import styles from "./quote-section.mobule.css";
+export default class QuoteSection extends Component {
+  render() {
+    return <section className={styles["quote-section-container"]}></section>;
+  }
+}
