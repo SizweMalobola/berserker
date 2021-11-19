c9d588a7a0bcf8aecde1a947a861f93b2da272ad
SizweMalobola <25reyals@gmail.com>
2021-11-19 06:54:21 +0200

    Add Hero UI component and its CSS module

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
 export class App extends Component {
@@ -12,6 +13,7 @@ export class App extends Component {
     return (
       <>
         <Navbar />
+        <Hero />
       </>
     );
   }
---
+++ src/components/hero/Hero.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./hero.module.css";
+
+function Hero() {
+  return <div className={styles["hero-container"]}></div>;
+}
+
+export default Hero;
