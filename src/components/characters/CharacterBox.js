80a32c1b0cc0c25ba0063792df322426bf05db2f
SizweMalobola <25reyals@gmail.com>
2021-11-20 11:36:56 +0200

    Add elements and assets to CharacterSection UI

--- src/components/characters/CharacterBox.js
+++ src/components/characters/CharacterBox.js
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./character-box.module.css";
 
-function Carousel() {
+function CharacterBox(props) {
   return (
     <>
-      <div className={styles["character-box-container"]}></div>
+      <div className={styles["character-box-container"]}>
+        <img
+          className={styles["character-img"]}
+          alt={`${props.name} character box`}
+          src={`${process.env.PUBLIC_URL}/assets/${props.imgName}`}
+        />
+        <h3>{props.name.toUpperCase()}</h3>
+      </div>
     </>
   );
 }
 
-export default Carousel;
+export default CharacterBox;
--- src/components/characters/CharacterSection.js
+++ src/components/characters/CharacterSection.js
@@ -1,11 +1,15 @@
 import React from "react";
 import SectionHeader from "../shared/SectionHeader";
+import CharacterBox from "./CharacterBox";
 import styles from "./characters-section.module.css";
 
 function CharacterSection() {
   return (
     <section className={styles["characters-container"]}>
       <SectionHeader name={"characters"} />
+      <CharacterBox name="guts" imgName="character_guts.png" />
+      <CharacterBox name="griffith" imgName="character_griffith.png" />
+      <CharacterBox name="casca" imgName="character_casca.png" />
     </section>
   );
 }
