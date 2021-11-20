d606b90cf0a1f5a48c42ede74d5f67ef132ab98f
SizweMalobola <25reyals@gmail.com>
2021-11-20 09:04:52 +0200

    Add CharacterBox UI component and styling module

---
+++ src/components/characters/CharacterBox.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./character-box.module.css";
+
+function Carousel() {
+  return (
+    <>
+      <div className={styles["character-box-container"]}></div>
+    </>
+  );
+}
+
+export default Carousel;
